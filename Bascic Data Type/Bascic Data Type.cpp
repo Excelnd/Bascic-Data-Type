350881092524727ea05f4b336e34e3bf1052a2b5
ihsan <ihsanmpm@gmail.com>
2017-12-15 15:44:14 +0530

    Add project files.

---
+++ Bascic Data Type/Bascic Data Type.cpp
@@ -0,0 +1,23 @@
+// Bascic Data Type.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+
+
+	int a = 3;
+	long b = 12345678912345;
+	char d = 'a';
+	float e = 334.23;
+	double f = 14049.30493;
+
+	printf("%d\n", a);
+	printf("%ld\n", b);
+	printf("%c\n", d);
+	printf("%f\n", e);
+	printf("%lf", f);
+	return 0;
+}
